fbd9931024d33bdc0504d61ae8bfb0a777054970
adilhossain053 <121199407+adilhossain053@users.noreply.github.com>
2024-07-09 20:08:05 +0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('beepButton').addEventListener('click', function() {
-    fetch('https://your-app-name.herokuapp.com/api/beep/play')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-});
